31d0af35dd3cf7e524a787e2a924692c6659c527
d-mishra-impelsys <52147996+d-mishra-impelsys@users.noreply.github.com>
2020-08-25 19:43:37 +0530

    Update StringIntern.java

--- String1/src/StringIntern.java
+++ String1/src/StringIntern.java
@@ -4,9 +4,9 @@ public class StringIntern
 	public static void main(String args[])
 	{
 		// Use of Intern Method:
-        // 1. If you want create a string object using new keyword referencing to String Pool
-        // 2. If you want create a string in pool memory using new keyword
-		//String ssss1="abc";
+               // 1. If you want create a string object using new keyword referencing to String Pool
+               // 2. If you want create a string in pool memory using new keyword
+		
 	
 		String s1=new String("ABC");
 		String s2="ABC";
@@ -25,18 +25,7 @@ public class StringIntern
 		String s6="XYZ";
 		System.out.println(s5==s6); // True or False 
 		System.out.println(s4==s6); // True or False 
-		/*
-		String s1 = "Java8";  
-        String s2 = s1.intern();  
-        String s3 = new String("Java8");  
-        String s4 = s3.intern();          
-        System.out.println(s1==s2); // True  
-        System.out.println(s1==s3); // False  
-        System.out.println(s1==s4); // True       
-        System.out.println(s2==s3); // False  
-        System.out.println(s2==s4); // True        
-        System.out.println(s3==s4); // False  
-        */
+		
         
         
 	}

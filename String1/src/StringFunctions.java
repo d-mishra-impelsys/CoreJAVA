f3cbace8dc29f7099c30c52b42f9c591d7e564ea
d-mishra-impelsys <52147996+d-mishra-impelsys@users.noreply.github.com>
2020-08-25 19:53:37 +0530

    Update StringFunctions.java

--- String1/src/StringFunctions.java
+++ String1/src/StringFunctions.java
@@ -4,18 +4,17 @@ public class StringFunctions
 	public static void main(String args[])
 	{
 		String str="Made In India";
-		System.out.println("Length:"+str.length());
-		System.out.println(str.charAt(3));
+		System.out.println("Length:"+str.length());//13
+		System.out.println(str.charAt(3)); // e
 		System.out.println(str.indexOf('d')); //2 
-		System.out.println(str.indexOf('I',6));
-		System.out.println(str.lastIndexOf('I'));
-		System.out.println(str.substring(3));
-		System.out.println("Sub:"+str.substring(3,9)); // only Two characters 5-3 , GL
-		System.out.println(str.substring(str.length()-4));
+		System.out.println(str.indexOf('I',6));//8
+		System.out.println(str.lastIndexOf('I'));//8
+		System.out.println(str.substring(3)); //e In India
+		System.out.println("Sub:"+str.substring(3,9)); // only Two characters 5-3 , Sub:e In I
+		System.out.println(str.substring(str.length()-4)); // ndia
 		
 		int posFromLast=4;
-		System.out.println("From Last:"+str.substring(str.length()-posFromLast));
+		System.out.println("From Last:"+str.substring(str.length()-posFromLast));//From Last:ndia
 		System.out.println(str);	
-		
 	}
 }
